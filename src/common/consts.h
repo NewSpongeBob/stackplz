16d17c10d81a284ca57f1daf77abad936ab878c9
hexl <hexl@hexl.com>
2025-07-17 11:51:05 +0800

    update buf size

--- src/common/consts.h
+++ src/common/consts.h
@@ -31,7 +31,7 @@
 #define PATH_MAX    4096
 #define MAX_STRING_SIZE    4096       // same as PATH_MAX
 #define MAX_BYTES_ARR_SIZE    4096       // same as PATH_MAX
-#define MAX_BUF_READ_SIZE    4096
+#define MAX_BUF_READ_SIZE    20480 
 #define ARGS_BUF_SIZE       32000
 
 // 配合 common_list 使用的 它们的间隔范围都是 0x400
