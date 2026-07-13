476eb2bcf7b88c2a03222d1e14ec8a8ce9a7960b
hexl <hexl@hexl.com>
2026-07-13 14:01:26 +0800

    resize * 22,2

--- src/common/consts.h
+++ src/common/consts.h
@@ -30,7 +30,7 @@
 #define MAX_PERCPU_BUFSIZE (1 << 15)  // set by the kernel as an upper bound
 #define PATH_MAX    4096
 #define MAX_STRING_SIZE    4096       // same as PATH_MAX
-#define MAX_BYTES_ARR_SIZE   1024*22       // same as PATH_MAX
+#define MAX_BYTES_ARR_SIZE   18432       // same as PATH_MAX
 #define MAX_BUF_READ_SIZE    1024*22 
 #define ARGS_BUF_SIZE       32000
 
